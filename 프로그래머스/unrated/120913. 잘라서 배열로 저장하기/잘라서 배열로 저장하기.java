294be9c9526b885884f790424c0d25124d9ccf2a
이찬미 <anytime0224@gmail.com>
2023-10-15 16:45:06 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.21 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,15 +1,14 @@
 class Solution {
     public String[] solution(String my_str, int n) {
         int my_str_length = my_str.length();
-        int answer_length = (int) Math.ceil((double) my_str.length() / n);
-        String[] answer = new String[answer_length];
+        int result_cnt = (int) Math.ceil((double) my_str_length / n);
+        String[] answer = new String[result_cnt];
         
-        for(int i = 0; i < answer_length; i++) {
-            if((i+1) * n >= my_str_length) {
-                answer[i] = my_str.substring(i * n, my_str_length);
-                break;
-            }
-            answer[i] = my_str.substring(i * n, (i+1) * n);
+        for(int i = 0; i < result_cnt; i++) {
+            int start = i * n;
+            int end = start + n <= my_str_length ? start + n : my_str_length;
+            
+            answer[i] = my_str.substring(start, end);
         }
         return answer;
     }
