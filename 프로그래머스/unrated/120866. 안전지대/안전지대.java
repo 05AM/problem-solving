e8c7c803fc9f17021cf05e1d9a02514152637203
이찬미 <anytime0224@gmail.com>
2023-10-20 00:59:56 +0900

    [level unrated] Title: 안전지대, Time: 3.05 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120866. 안전지대/안전지대.java
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.java
@@ -2,42 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board) {
-        int[][] newBoard = new int[board.length][board.length];
-
         for(int row = 0; row < board.length; row++) {
             for(int col = 0; col < board.length; col++) {
                 if(board[row][col] == 1) {
-                    // 자기자신
-                    newBoard[row][col] = 1;
-
-                    // -1 행
-                    if(row - 1 >= 0) {
-                        newBoard[row - 1][col] = 2;
-                        if(col - 1 >= 0)
-                            newBoard[row - 1][col - 1] = 2;
-                        if(col + 1 < board.length)
-                            newBoard[row - 1][col + 1] = 2;
-                    }
-                    // 0 행
-                    if(col - 1 >= 0)
-                        newBoard[row][col - 1] = 2;
-                    if(col + 1 < board.length)
-                        newBoard[row][col + 1] = 2;
-
-                    // 1 행
-                    if(row + 1 < board.length) {
-                        newBoard[row + 1][col] = 2;
-                        if(col - 1 >= 0)
-                            newBoard[row + 1][col - 1] = 2;
-                        if(col + 1 < board.length)
-                            newBoard[row + 1][col + 1] = 2;
-                    }
+                    setArea(board, row, col);
                 }
             }
         }
 
-        return (int) Arrays.stream(newBoard)
-                .mapToInt(arr -> (int) Arrays.stream(arr).filter(i -> i == 0).count())
-                .sum();
+        return (int) Arrays.stream(board)
+            .mapToInt(arr -> (int) Arrays.stream(arr).filter(i -> i == 0).count())
+            .sum();
+    }
+    
+    public void setArea(int[][] board, int row, int col) {
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                try {
+                    if(board[row + i][col + j] == 0)
+                        board[row + i][col + j] = 2;
+                } catch(Exception e) {
+                }
+            }
+        }
     }
 }
