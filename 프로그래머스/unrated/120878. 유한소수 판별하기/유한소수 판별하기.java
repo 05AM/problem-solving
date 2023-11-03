6b5a97843df970536a1ff7a34332a416962f4f5a
이찬미 <anytime0224@gmail.com>
2023-11-04 02:45:55 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,22 +1,26 @@
 class Solution {
     public int solution(int a, int b) {
-        int limit = Math.min(a, b);
-
-        for(int i = 1; i <= limit; i++) {
-            if((a % i == 0) && (b % i == 0)) {
-                a /= i;
-                b /= i;
-            }
-        }
-
-        while(b % 5 == 0) {
-            b /= 5;
-        }
-
-        while(b % 2 == 0) {
-            b /= 2;
-        }
+        b /= gcd(a, b);
         
-        return b == 1 ? 1 : 2;
+        while (b != 1) {
+            if (b % 5 == 0) {
+                b /= 5;
+                continue;
+            }
+            
+            if (b % 2 == 0) {
+                b /= 2;
+                continue;
+            }
+            return 2;
+        }
+        return 1;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
