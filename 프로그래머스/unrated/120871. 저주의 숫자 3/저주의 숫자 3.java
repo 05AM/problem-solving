03846fff4cccb12c4dd160bc0e278e621f9206b4
이찬미 <anytime0224@gmail.com>
2023-11-03 23:01:00 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.10 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,15 +1,11 @@
-import java.util.regex.Pattern;
-
 class Solution {
     public int solution(int n) {
         int num3x = 0;
 
         for(int i = 1; i <= n; i++) {
-            num3x++;
-
-            while(validate(num3x)) {
-                num3x++;
-            }
+            do {
+                num3x++; 
+            } while (validate(num3x));
         }
 
         return num3x;
