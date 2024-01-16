2e3e23e5d8f5eced02ac91a3a64f445ad5768a95
이찬미 <anytime0224@gmail.com>
2024-01-16 19:41:24 +0900

    Create 가위바위보.java

---
+++ Java 알고리즘 문제풀이 /배열/가위바위보.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private String[] solution(int n, int[] optionsA, int[] optionsB) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            int a = optionsA[i];
+            int b = optionsB[i];
+
+            if (a == b) {
+                answer[i] = "D";
+            } else if ((a == 1 && b == 3) || (a == 2 && b == 1) || (a == 3 && b == 2)) {
+                answer[i] = "A";
+            } else {
+                answer[i] = "B";
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] optionsA = new int[n];
+        int[] optionsB = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            optionsA[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            optionsB[i] = in.nextInt();
+        }
+
+        Arrays.stream(T.solution(n, optionsA, optionsB))
+                .forEach(System.out::println);
+    }
+}
