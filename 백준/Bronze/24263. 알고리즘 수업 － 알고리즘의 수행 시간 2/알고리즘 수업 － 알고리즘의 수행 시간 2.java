b5b03d0f06cb721c7b770a2bef1d9326f71f1289
이찬미 <anytime0224@gmail.com>
2024-07-29 21:35:37 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        solution(n);
+    }
+
+    static void solution(int n) {
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
