eb0ee20559f5e4491208058ff358820fbb12a9df
이찬미 <anytime0224@gmail.com>
2024-07-30 17:31:25 +0900

    [Bronze II] Title: 분해합, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        System.out.println(solution(n));
+    }
+
+    private static int solution(int n) {
+        int size = 9 * String.valueOf(n).length();
+
+        for (int i = n - size; i < n; i++) {
+            if (getDecompositionSum(i) == n) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int getDecompositionSum(int n) {
+        String str = String.valueOf(n);
+
+        int digitSum = 0;
+        for (char digit : str.toCharArray()) {
+            digitSum += Character.getNumericValue(digit);
+        }
+
+        return n + digitSum;
+    }
+}
