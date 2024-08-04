c1745f20c8fadc25378a55a53b630c08e04a00f6
이찬미 <anytime0224@gmail.com>
2024-08-04 21:50:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        System.out.println(solution(n));
+
+    }
+
+    private static int solution(int n) {
+        int cnt_3 = 0;
+        int cnt_5 = n / 5;
+        int rest = n % 5;
+
+        if (rest == 0) {
+            return cnt_5;
+        }
+
+        while (cnt_5 >= 0) {
+            if (rest % 3 == 0) {
+                cnt_3 = rest / 3;
+                break;
+            } else {
+                cnt_5--;
+                rest += 5;
+            }
+        }
+
+        if (cnt_5 >= 0) {
+            return cnt_3 + cnt_5;
+        } else {
+            return -1;
+        }
+    }
+}
