5836dfaf16368cbaccdf175a72ca820eebf04ef2
이찬미 <anytime0224@gmail.com>
2024-09-15 13:25:49 +0900

    [Silver II] Title: 연속합, Time: 228 ms, Memory: 25272 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,25 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        
-        int maxSum = arr[0];
-        int currentSum = arr[0];
-        
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+        int[] sequence = Arrays.stream(in.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int max = solution(n, sequence);
+
+        System.out.println(max);
+    }
+
+    private static int solution(int n, int[] sequence) {
+        int currentSum = sequence[0];
+        int max = sequence[0];
+
         for (int i = 1; i < n; i++) {
-            currentSum = Math.max(arr[i], currentSum + arr[i]);
-            maxSum = Math.max(maxSum, currentSum);
+            currentSum = Math.max(currentSum + sequence[i], sequence[i]);
+            max = Math.max(max, currentSum);
         }
-        
-        System.out.println(maxSum);
-        sc.close();
+
+        return max;
     }
 }
