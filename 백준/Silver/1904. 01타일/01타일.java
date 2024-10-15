e4e4c9241ee1e5581e34f5f890fcbf0d1ff414c5
이찬미 <anytime0224@gmail.com>
2024-10-15 23:24:45 +0900

    [Silver III] Title: 01타일, Time: 180 ms, Memory: 21604 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-
     static int[] dp;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
 
+        int n = in.nextInt();
         dp = new int[n + 1];
 
-        dp[1] = 1;
+        System.out.println(solution(n));
+    }
 
-        if (n > 1) {
-            dp[2] = 2;
+    private static int solution(int n) {
+        if (n == 1) {
+            return 1;
         }
+
+        dp[1] = 1;
+        dp[2] = 2;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
         }
 
-        System.out.println(dp[n]);
+        return dp[n];
     }
 }
