5ccde27efb7e589a6e417bd056db280535f244b6
이찬미 <anytime0224@gmail.com>
2024-10-16 16:02:37 +0900

    [Silver I] Title: RGB거리, Time: 216 ms, Memory: 20464 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,41 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
+    static int[][] dp;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int n = in.nextInt();
-        int[][] costs = new int[n][3];
 
-        for (int i = 0; i < n; i++) {
+        dp = new int[n + 1][3];
+        int[][] costs = new int[n + 1][3];
+
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j < 3; j++) {
                 costs[i][j] = in.nextInt();
             }
         }
 
-        int min = solution(n, costs);
-        System.out.println(min);
+        System.out.println(solution(n, costs));
     }
 
     private static int solution(int n, int[][] costs) {
-        int[][] dp = new int[n][3];
-
-        dp[0][0] = costs[0][0];
-        dp[0][1] = costs[0][1];
-        dp[0][2] = costs[0][2];
-
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j < 3; j++) {
-                int minCost = Math.min(dp[i - 1][(j + 1) % 3], dp[i - 1][(j + 2) % 3]);
-
-                dp[i][j] = costs[i][j] + minCost;
+                dp[i][j] = costs[i][j] + Math.min(dp[i - 1][(j + 1) % 3], dp[i - 1][(j + 2) % 3]);
             }
         }
 
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < 3; i++) {
-            min = Math.min(min, dp[n - 1][i]);
+            min = Math.min(min, dp[n][i]);
         }
 
         return min;
