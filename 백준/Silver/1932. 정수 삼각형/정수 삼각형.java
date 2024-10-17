aacfd7c8a2e52c3c9edb6ce481b0d41b4d863bde
이찬미 <anytime0224@gmail.com>
2024-10-17 13:30:47 +0900

    [Silver I] Title: 정수 삼각형, Time: 748 ms, Memory: 133480 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,49 +1,35 @@
+
 import java.util.Scanner;
 
 public class Main {
-
+    
+    static int[][] dp;
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        
         int n = in.nextInt();
-        int[][] costs = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                costs[i][j] = in.nextInt();
+        int[][] triangle = new int[n][n];
+        dp = new int[n + 1][n + 1];
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                triangle[i][j] = in.nextInt();
             }
         }
-
-        int min = solution(n, costs);
-        System.out.println(min);
+        
+        int answer = solution(n, triangle);
+        System.out.println(answer);
     }
-
-    private static int solution(int n, int[][] costs) {
-        int[][] dp = new int[n][n];
-
-        dp[0][0] = costs[0][0];
-
-        for (int row = 1; row < n; row++) {
-            for (int col = 0; col <= row; col++) {
-                if (col == 0) {
-                    dp[row][col] = dp[row - 1][col] + costs[row][col];
-                    continue;
-                }
-
-                if (col == row) {
-                    dp[row][col] = dp[row - 1][col - 1] + costs[row][col];
-                    continue;
-                }
-
-                dp[row][col] = Math.max(dp[row - 1][col], dp[row - 1][col - 1]) + costs[row][col];
+    
+    private static int solution(int n, int[][] triangle) {
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                int max = Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+                dp[i][j] = triangle[i][j] + max;
             }
         }
-
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, dp[n - 1][i]);
-        }
-
-        return max;
+        
+        return dp[0][0];
     }
 }
