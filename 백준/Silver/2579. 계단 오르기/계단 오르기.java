b7652f2c1ea13b40e68953a03002aa4ef5aaf960
이찬미 <anytime0224@gmail.com>
2024-10-17 14:51:00 +0900

    [Silver III] Title: 계단 오르기, Time: 180 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
-
     static int[] dp;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int n = in.nextInt();
-        int[] scores = new int[n + 1];
         dp = new int[n + 1];
+        int[] costs = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
-            scores[i] = in.nextInt();
+            costs[i] = in.nextInt();
         }
 
-        solution(n, scores);
-        System.out.println(dp[n]);
+        System.out.println(solution(n, costs));
     }
 
-    private static void solution(int n, int[] scores) {
+    private static int solution(int n, int[] costs) {
         if (n == 1) {
-            dp[1] = scores[1];
-            return;
+            return costs[1];
         }
         
-        dp[1] = scores[1];
-        dp[2] = scores[1] + scores[2];
+        dp[1] = costs[1];
+        dp[2] = dp[1] + costs[2];
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 2], dp[i - 3] + scores[i - 1]) + scores[i];
+            int max = Math.max(dp[i - 3] + costs[i - 1], dp[i - 2]);
+            dp[i] = costs[i] + max;
         }
+
+        return dp[n];
     }
 }
