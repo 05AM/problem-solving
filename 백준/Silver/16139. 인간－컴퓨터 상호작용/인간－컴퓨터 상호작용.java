41f5625f7a75563e1a84f664359371fe0e90e52d
이찬미 <anytime0224@gmail.com>
2024-10-23 15:25:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 848 ms, Memory: 114412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,37 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     static int[][] prefixSum;
     static final int ALPHABETS_CNT = 26;
     static final char LOW_A = 'a';
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = in.nextLine();
+        String s = in.readLine();
         prefixSum = new int[ALPHABETS_CNT][s.length() + 1];
         solution(s);
 
-        int q = in.nextInt();
+        int q = Integer.parseInt(in.readLine());
         for (int i = 0; i < q; i++) {
-            char alpha = in.next().charAt(0);
-            int l = in.nextInt();
-            int r = in.nextInt();
+            String[] input = in.readLine().split(" ");
+
+            char alpha = input[0].charAt(0);
+            int l = Integer.parseInt(input[1]);
+            int r = Integer.parseInt(input[2]);
 
             int index = alpha - LOW_A;
-            System.out.println(prefixSum[index][r + 1] - prefixSum[index][l]);
+            out.write(prefixSum[index][r + 1] - prefixSum[index][l] + "\n");
         }
+        out.flush();
 
+        in.close();
+        out.close();
     }
 
     private static void solution(String s) {
         for (int i = 0; i < ALPHABETS_CNT; i++) {
             for (int j = 0; j < s.length(); j++) {
-                if(s.charAt(j) == (LOW_A + i)) {
-                    prefixSum[i][j + 1] = prefixSum[i][j] + 1;
-                } else {
-                    prefixSum[i][j + 1] = prefixSum[i][j];
-                }
+                int sum = s.charAt(j) == (LOW_A + i) ? 1 : 0;
+                prefixSum[i][j + 1] = prefixSum[i][j] + sum;
             }
         }
     }
