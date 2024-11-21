357a2983a5b3b894e7a139a7e8175c9bbbf15000
이찬미 <anytime0224@gmail.com>
2024-11-21 23:32:40 +0900

    [Silver IV] Title: 수 찾기, Time: 1660 ms, Memory: 190692 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        int m = in.nextInt();
+        for (int i = 0; i < m; i++) {
+            int target = in.nextInt();
+            int result = solution(nums, target) ? 1 : 0;
+
+            System.out.println(result);
+        }
+    }
+
+    private static boolean solution(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
