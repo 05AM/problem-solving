7c30daabc1ac31fc00fcc194b1e61c2b1722944b
이찬미 <anytime0224@gmail.com>
2024-11-24 23:16:08 +0900

    [Silver II] Title: 최소 힙, Time: 1404 ms, Memory: 119076 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+
+            if (x != 0) {
+                pq.offer(x);
+            } else {
+                int value = pq.isEmpty() ? 0 : pq.poll();
+                System.out.println(value);
+            }
+        }
+
+        in.close();
+    }
+}
