261d82b8de289397e90444b0cda0e90d1aa30fe8
이찬미 <anytime0224@gmail.com>
2024-12-13 21:15:06 +0900

    [Silver II] Title: 종이의 개수, Time: 3252 ms, Memory: 350976 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -23,32 +23,26 @@ public class Main {
         in.close();
     }
 
-    private static void solution(int n, int row, int col) {
-        if (isAllSame(n, row, col)) {
+    private static void solution(int size, int row, int col) {
+        if (checkSame(size, row, col)) {
             int value = paper[row][col];
             results[value + 1]++;
             return;
         }
 
-        int newSize = n / 3;
-        solution(newSize, row, col);
-        solution(newSize, row + newSize, col);
-        solution(newSize, row + 2 * newSize, col);
-
-        solution(newSize, row, col + newSize);
-        solution(newSize, row + newSize, col + newSize);
-        solution(newSize, row + 2 * newSize, col + newSize);
-
-        solution(newSize, row, col + 2 * newSize);
-        solution(newSize, row + newSize, col + 2 * newSize);
-        solution(newSize, row + 2 * newSize, col + 2 * newSize);
+        int newSize = size / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                solution(newSize, row + i * newSize, col + j * newSize);
+            }
+        }
     }
 
-    private static boolean isAllSame(int n, int row, int col) {
+    private static boolean checkSame(int size, int row, int col) {
         int value = paper[row][col];
 
-        for (int i = row; i < row + n; i++) {
-            for (int j = col; j < col + n; j++) {
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
                 if (paper[i][j] != value) {
                     return false;
                 }
