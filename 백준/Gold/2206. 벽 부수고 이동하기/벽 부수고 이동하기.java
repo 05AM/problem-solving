3f173c2ba3f34c752155112c483483bcddcf9609
이찬미 <anytime0224@gmail.com>
2025-01-02 11:38:36 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1224 ms, Memory: 204136 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -6,9 +6,9 @@ class Node {
     int row;
     int col;
     int cnt;
-    boolean canBreak;
+    int canBreak;
 
-    public Node(int row, int col, int cnt, boolean canBreak) {
+    public Node(int row, int col, int cnt, int canBreak) {
         this.row = row;
         this.col = col;
         this.cnt = cnt;
@@ -20,8 +20,7 @@ public class Main {
     private static final int START_ROW = 1;
     private static final int START_COL = 1;
 
-    private static final int[] directionRow = {1, 0, -1, 0};
-    private static final int[] directionCol = {0, 1, 0, -1};
+    private static final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -41,12 +40,12 @@ public class Main {
     }
 
     private static int getShortestPath(int n, int m, int[][] matrix) {
-        boolean[][] visitedWithoutBreaking = new boolean[n + 1][m + 1];
-        boolean[][] visitedAfterBreaking = new boolean[n + 1][m + 1];
+        // [벽을 부순 여부][행][열]
+        boolean[][][] isVisited = new boolean[2][n + 1][m + 1];
 
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(START_ROW, START_COL, 1, true));
-        visitedWithoutBreaking[START_ROW][START_COL] = true;
+        queue.add(new Node(START_ROW, START_COL, 1, 1));
+        isVisited[0][START_ROW][START_COL] = true;
 
         int min = -1;
         while (!queue.isEmpty()) {
@@ -58,26 +57,21 @@ public class Main {
             }
 
             for (int i = 0; i < 4; i++) {
-                int nextRow = node.row + directionRow[i];
-                int nextCol = node.col + directionCol[i];
+                int nextRow = node.row + directions[i][0];
+                int nextCol = node.col + directions[i][1];
 
                 if (nextRow > n || nextRow <= 0 || nextCol > m || nextCol <= 0) {
                     continue;
                 }
 
-                if (matrix[nextRow][nextCol] == 1) {
-                    if (node.canBreak && !visitedAfterBreaking[nextRow][nextCol]) {
-                        visitedAfterBreaking[nextRow][nextCol] = true;
-                        queue.add(new Node(nextRow, nextCol, node.cnt + 1, false));
-                    }
-                } else {
-                    if (node.canBreak && !visitedWithoutBreaking[nextRow][nextCol]) {
-                        visitedWithoutBreaking[nextRow][nextCol] = true;
-                        queue.add(new Node(nextRow, nextCol, node.cnt + 1, node.canBreak));
-                    } else if (!node.canBreak && !visitedAfterBreaking[nextRow][nextCol]) {
-                        visitedAfterBreaking[nextRow][nextCol] = true;
-                        queue.add(new Node(nextRow, nextCol, node.cnt + 1, node.canBreak));
-                    }
+                int wall = matrix[nextRow][nextCol];
+                if (wall == 0 && !isVisited[node.canBreak][nextRow][nextCol]) {
+                    isVisited[node.canBreak][nextRow][nextCol] = true;
+                    queue.add(new Node(nextRow, nextCol, node.cnt + 1, node.canBreak));
+                }
+                else if(wall == 1 && node.canBreak == 1 && !isVisited[1][nextRow][nextCol]){
+                    isVisited[node.canBreak][nextRow][nextCol] = true;
+                    queue.add(new Node(nextRow, nextCol, node.cnt + 1, 0));
                 }
             }
         }
