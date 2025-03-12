cf986cc08a46b5aafd45c3fa4b445c2a8ba78b8c
이찬미 <anytime0224@gmail.com>
2025-03-12 11:18:33 +0900

    Update 도넛과 막대 그래프.java

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -9,28 +9,28 @@ class Solution {
         int barCount = 0;
         int eightCount = 0;
 
-        // 간선 정보를 통해 각 정점의 진출 차수와 진입 차수를 계산합니다.
+        // 간선 정보를 통해 각 정점의 진출 차수와 진입 차수를 계산
         for (int[] edge : edges) {
             outDegree.put(edge[0], outDegree.getOrDefault(edge[0], 0) + 1);
             inDegree.put(edge[1], inDegree.getOrDefault(edge[1], 0) + 1);
         }
 
-        // 각 정점을 순회하며 그래프의 특징을 파악합니다.
+        // 그래프 순회
         for (int node : outDegree.keySet()) {
             int out = outDegree.get(node);
             int in = inDegree.getOrDefault(node, 0);
 
-            // 생성된 정점은 진입 차수가 0이고 진출 차수가 2 이상입니다.
+            // 생성된 정점은 진입 차수가 0이고 진출 차수가 2 이상
             if (in == 0 && out >= 2) {
                 createdVertex = node;
             }
-            // 8자 모양 그래프의 중앙 정점은 진입 차수와 진출 차수가 각각 2 이상입니다.
+            // 8자 모양 그래프의 중앙 정점은 진입 차수와 진출 차수가 각각 2 이상
             else if (in >= 2 && out == 2) {
                 eightCount++;
             }
         }
 
-        // 막대 모양 그래프의 끝 정점은 진출 차수가 0이고 진입 차수가 1 이상입니다.
+        // 막대 모양 그래프의 끝 정점은 진출 차수가 0이고 진입 차수가 1 이상
         for (int node : inDegree.keySet()) {
             int out = outDegree.getOrDefault(node, 0);
             int in = inDegree.get(node);
@@ -40,9 +40,9 @@ class Solution {
             }
         }
 
-        // 전체 그래프의 수는 생성된 정점의 진출 차수와 같습니다.
+        // 전체 그래프의 수는 생성된 정점의 진출 차수와 같음
         int totalGraphs = outDegree.get(createdVertex);
-        // 도넛 모양 그래프의 수는 전체 그래프 수에서 막대와 8자 그래프 수를 뺀 값입니다.
+        // 도넛 모양 그래프의 수는 전체 그래프 수에서 막대와 8자 그래프 수를 뺀 값
         donutCount = totalGraphs - barCount - eightCount;
 
         return new int[]{createdVertex, donutCount, barCount, eightCount};
