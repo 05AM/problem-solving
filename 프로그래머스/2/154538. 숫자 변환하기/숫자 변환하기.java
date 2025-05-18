3a55cbf8350fdfbc3b5865d899227e73d91316ac
이찬미 <anytime0224@gmail.com>
2025-05-19 00:13:38 +0900

    [level 2] Title: 숫자 변환하기, Time: 116.91 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,46 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-        int answer = -1;
-
-        boolean[] isVisited = new boolean[y + 1];
+                boolean[] isVisited = new boolean[y + 1];
         Queue<Integer[]> queue = new LinkedList<>();
-        queue.add(new Integer[]{x, 0});
+        queue.add(new Integer[] {x, 0});
         isVisited[x] = true;
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             Integer[] current = queue.poll();
             int currentX = current[0];
             int trial = current[1];
 
-            if(currentX == y) {
-                if (answer == -1) {
-                    answer = trial;
-                } else {
-                    answer = Math.min(answer, trial);
-
-                    if (trial > answer) {
-                        break;
-                    }
-                }
+            if (currentX == y) {
+                return trial;
             }
 
-            if(currentX * 3 <= y && !isVisited[currentX * 3]) {
+            if (currentX * 3 <= y && !isVisited[currentX * 3]) {
                 isVisited[currentX * 3] = true;
-                queue.add(new Integer[]{currentX * 3, trial + 1});
+                queue.add(new Integer[] {currentX * 3, trial + 1});
             }
 
-            if(currentX * 2 <= y && !isVisited[currentX * 2]) {
+            if (currentX * 2 <= y && !isVisited[currentX * 2]) {
                 isVisited[currentX * 2] = true;
-                queue.add(new Integer[]{currentX * 2, trial + 1});
+                queue.add(new Integer[] {currentX * 2, trial + 1});
             }
 
-            if(currentX + n <= y && !isVisited[currentX + n]) {
+            if (currentX + n <= y && !isVisited[currentX + n]) {
                 isVisited[currentX + n] = true;
-                queue.add(new Integer[]{currentX + n, trial + 1});
+                queue.add(new Integer[] {currentX + n, trial + 1});
             }
         }
 
-        return answer;
+        return -1;
     }
 }
