1955305c06c2a6d2d1c71aa652a7ce6ab7282053
이찬미 <anytime0224@gmail.com>
2025-05-26 00:23:04 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1868 ms, Memory: 80200 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,68 +1,92 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    static int[] dRow = {0, 1, 0, -1};
-    static int[] dCol = {1, 0, -1, 0};
+    static int[] moveRow = {0, 1, 0, -1};
+    static int[] moveCol = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = in.readLine().split(" ");
-        int n = Integer.parseInt(line[0]);
-        int m = Integer.parseInt(line[1]);
+
+        // 입력
+        String[] firstLine = in.readLine().split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int m = Integer.parseInt(firstLine[1]);
 
         int[][] map = new int[n][m];
-        int[][] dist = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
-
-        int startRow = -1, startCol = -1;
 
         for (int i = 0; i < n; i++) {
-            String[] parts = in.readLine().split(" ");
+            map[i] = Arrays.stream(in.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        // 결과 구하기
+        int[][] dist = solution(n, m, map);
+
+        // 출력
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(parts[j]);
-                if (map[i][j] == 2) {
-                    startRow = i;
-                    startCol = j;
+                System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] solution(int n, int m, int[][] map) {
+        int targetRow = -1;
+        int targetCol = -1;
+        int[][] dist = new int[n][m];
+
+        // 목표지점 찾으며 정해진 예정 결과 값 채우기
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                int type = map[row][col];
+
+                if (type == 0) {
+                    dist[row][col] = 0;
+                } else if (type == 1) {
+                    dist[row][col] = -1;
+                } else if (type == 2) {
+                    targetRow = row;
+                    targetCol = col;
                 }
-                dist[i][j] = -1;
             }
         }
 
+        boolean[][] visited = new boolean[n][m];
+
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{startRow, startCol});
-        visited[startRow][startCol] = true;
-        dist[startRow][startCol] = 0;
+        visited[targetRow][targetCol] = true;
+        queue.add(new int[] {targetRow, targetCol, 0});
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int row = current[0];
             int col = current[1];
 
-            for (int d = 0; d < 4; d++) {
-                int newRow = row + dRow[d];
-                int newCol = col + dCol[d];
+            // 이동
+            for (int i = 0; i < 4; i++) {
+                int nextRow = row + moveRow[i];
+                int nextCol = col + moveCol[i];
 
-                if (newRow < 0 || newRow >= n || newCol < 0 || newCol >= m)
+                if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= m || visited[nextRow][nextCol]) {
                     continue;
+                }
 
-                if (!visited[newRow][newCol] && map[newRow][newCol] == 1) {
-                    visited[newRow][newCol] = true;
-                    dist[newRow][newCol] = dist[row][col] + 1;
-                    queue.add(new int[]{newRow, newCol});
+                if (map[nextRow][nextCol] == 1) {
+                    dist[nextRow][nextCol] = dist[row][col] + 1;
+                    visited[nextRow][nextCol] = true;
+                    queue.add(new int[] {nextRow, nextCol});
                 }
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 0) {
-                    dist[i][j] = 0;
-                }
-                System.out.print(dist[i][j] + " ");
-            }
-            System.out.println();
-        }
+        return dist;
     }
 }
