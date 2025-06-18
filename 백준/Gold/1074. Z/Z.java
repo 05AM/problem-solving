5eb91383bc19c3281be1d4b771ec9ea2f33b2b89
이찬미 <anytime0224@gmail.com>
2025-06-18 18:45:25 +0900

    [Gold V] Title: Z, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-    private static int answer = -1;
-    private static boolean isFound;
+    private static int answer = 0;
 
     private static int targetRow;
     private static int targetCol;
-
-    private static final int[] moveRow = {0, 0, 1, 1};
-    private static final int[] moveCol = {0, 1, 0, 1};
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -18,41 +14,35 @@ public class Main {
         targetRow = in.nextInt();
         targetCol = in.nextInt();
 
-        int side = (int) Math.pow(2, n);
-        solution(n, side, 0, 0);
+        int side = 1 << n;
+        solution(side, 0, 0);
 
         System.out.println(answer);
     }
 
-    private static void solution(int n, int side, int startRow, int startCol) {
-        if (isFound) {
+    private static void solution(int side, int row, int col) {
+        if (side == 1) {
             return;
         }
 
-        // 범위 안에 없으면 사이즈 더하고 빠져나오기
-        if (targetRow < startRow || targetRow > startRow + side || targetCol < startCol || targetCol > startCol + side) {
-            answer += side * side;
-            return;
+        int half = side / 2;
+        int blockSize = half * half;
+
+        if (targetRow < row + half && targetCol < col + half) {
+            // 1사분면
+            solution(half, row, col);
+        } else if (targetRow < row + half) {
+            // 2사분면
+            answer += blockSize;
+            solution(half, row, col + half);
+        } else if (targetCol < col + half) {
+            // 3사분면
+            answer += 2 * blockSize;
+            solution(half, row + half, col);
+        } else {
+            // 4사분면
+            answer += 3 * blockSize;
+            solution(half, row + half, col + half);
         }
-
-        if (n == 1) {
-            for (int i = 0; i < 4; i++) {
-                answer++;
-
-                if (startRow + moveRow[i] == targetRow && startCol + moveCol[i] == targetCol) {
-                    isFound = true;
-                    return;
-                }
-            }
-            return;
-        }
-
-        int nextSize = n - 1;
-        int nextSide = (int) Math.pow(2, nextSize);
-
-        solution(nextSize, nextSide, startRow, startCol);
-        solution(nextSize, nextSide, startRow, startCol + nextSide);
-        solution(nextSize, nextSide, startRow + nextSide, startCol);
-        solution(nextSize, nextSide, startRow + nextSide, startCol + nextSide);
     }
 }
