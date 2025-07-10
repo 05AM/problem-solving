50781ae4a1556efd8822f6ce61bcd1197d2579ea
이찬미 <anytime0224@gmail.com>
2025-07-10 23:41:03 +0900

    [Silver I] Title: 스티커, Time: 704 ms, Memory: 138056 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(in.readLine());
+        while (T-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+
+            int[][] stickers = new int[2][n];
+            stickers[0] = Arrays.stream(in.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            stickers[1] = Arrays.stream(in.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            if (n < 2) {
+                System.out.println(Math.max(stickers[0][0], stickers[1][0]));
+                continue;
+            }
+
+            int[][] dp = new int[2][n];
+            dp[0][0] = stickers[0][0];
+            dp[1][0] = stickers[1][0];
+
+            dp[0][1] = stickers[1][0] + stickers[0][1];
+            dp[1][1] = stickers[0][0] + stickers[1][1];
+
+            for (int i = 2; i < n; i++) {
+                dp[0][i] = stickers[0][i] + Math.max(dp[1][i - 1], Math.max(dp[0][i - 2], dp[1][i - 2]));
+                dp[1][i] = stickers[1][i] + Math.max(dp[0][i - 1], Math.max(dp[0][i - 2], dp[1][i - 2]));
+            }
+
+            System.out.println(Math.max(dp[0][n - 1], dp[1][n - 1]));
+        }
+    }
+}
