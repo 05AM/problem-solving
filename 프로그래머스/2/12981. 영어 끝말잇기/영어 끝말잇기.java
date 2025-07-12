a1057fbe8c54f24dac794e63dc56713b84de018f
이찬미 <anytime0224@gmail.com>
2025-07-12 23:23:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,33 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-int[] answer = new int[2];
+        int[] answer = new int[2];
 
         Set<String> used = new HashSet<>();
-        int order = 2;
-        int round = 1;
-
         used.add(words[0]);
-        String last = words[0];
 
         for(int i = 1; i < words.length; i++) {
-            String word = words[i];
+            String prev = words[i - 1];
+            String current = words[i];
 
-            char lastChar = last.charAt(last.length() - 1);
-            char firstChar = word.charAt(0);
+            char lastChar = prev.charAt(prev.length() - 1);
+            char firstChar = current.charAt(0);
 
-            if (used.contains(word) || lastChar != firstChar) {
-                return new int[] { order, round };
+            if (used.contains(current) || lastChar != firstChar) {
+                return new int[] { (i % n) + 1, (i / n) + 1 };
             }
 
-            used.add(word);
-            last = word;
-            order = (order + 1) % (n + 1);
-
-            if (order == 0) {
-                order++;
-                round++;
-            }
+            used.add(current);
         }
 
         return answer;
