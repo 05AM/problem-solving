dd0af9fe241983c8f00e78abc146d16531a0577a
이찬미 <anytime0224@gmail.com>
2025-08-08 00:39:34 +0900

    Update 서강그라운드.java 플로이드 워샬 알고리즘으로 풀기

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,34 +1,12 @@
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
 
-    static class Edge {
-        int to;
-        int dist;
-
-        public Edge(int to, int dist) {
-            this.to = to;
-            this.dist = dist;
-        }
-    }
-
-    static class Path {
-        int to;
-        int totalDist;
-
-        public Path(int to, int totalDist) {
-            this.to = to;
-            this.totalDist = totalDist;
-        }
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(new InputStreamReader(System.in));
+        final int INF = 9999999;
 
         int n = in.nextInt();   // 지역 개수
         int m = in.nextInt();   // 수색 범위
@@ -39,9 +17,11 @@ public class Main {
             items[i] = in.nextInt();
         }
 
-        ArrayList<Edge>[] graph = new ArrayList[n + 1];
+        // 초기화
+        int[][] distances = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            Arrays.fill(distances[i], INF);
+            distances[i][i] = 0;
         }
 
         for (int i = 1; i <= r; i++) {
@@ -49,48 +29,32 @@ public class Main {
             int b = in.nextInt();
             int l = in.nextInt();
 
-            graph[a].add(new Edge(b, l));
-            graph[b].add(new Edge(a, l));
+            distances[a][b] = Math.min(distances[a][b], l);
+            distances[b][a] = Math.min(distances[b][a], l);
         }
 
-        System.out.println(solution(n, m, items, graph));
+        System.out.println(solution(n, m, items, distances));
     }
 
-    private static int solution(int n, int m, int[] items, ArrayList<Edge>[] graph) {
+    private static int solution(int n, int m, int[] items, int[][] distances) {
+        for (int k = 1; k <= n; k++) {
+            for (int row = 1; row <= n; row++) {
+                for (int col = 1; col <= n; col++) {
+                    distances[row][col] = Math.min(distances[row][col], distances[row][k] + distances[k][col]);
+                }
+            }
+        }
+
         int answer = Integer.MIN_VALUE;
+        for (int from = 1; from <= n; from++) {
+            int itemCnt = 0;
 
-        for (int i = 1; i <= n; i++) {
-            PriorityQueue<Path> pq = new PriorityQueue<>(Comparator.comparing((Path path) -> path.totalDist));
-            int[] distances = new int[n + 1];
-            Arrays.fill(distances, Integer.MAX_VALUE);
-
-            pq.add(new Path(i, 0));
-
-            while (!pq.isEmpty()) {
-                Path current = pq.poll();
-
-                if (current.totalDist > distances[current.to]) {
-                    continue;
-                }
-                distances[current.to] = current.totalDist;
-
-                for (Edge edge : graph[current.to]) {
-                    int newDist = current.totalDist + edge.dist;
-
-                    if (newDist <= m && newDist < distances[edge.to]) {
-                        pq.add(new Path(edge.to, newDist));
-                    }
+            for (int to = 1; to <= n; to++) {
+                if (distances[from][to] <= m) {
+                    itemCnt += items[to];
                 }
             }
-
-            int totalItemCnt = 0;
-            for (int j = 1; j <= n; j++) {
-                if (distances[j] != Integer.MIN_VALUE && distances[j] <= m) {
-                    totalItemCnt += items[j];
-                }
-            }
-
-            answer = Math.max(answer, totalItemCnt);
+            answer = Math.max(answer, itemCnt);
         }
 
         return answer;
