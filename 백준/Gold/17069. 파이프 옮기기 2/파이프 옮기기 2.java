74b20e21dde20ef1cb4e6ab8adc2eddef4c0faad
이찬미 <anytime0224@gmail.com>
2025-08-31 18:38:06 +0900

    Update 파이프 옮기기 2.java

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,30 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    private static final int HORIZONTAL = 0;
+    private static final int VERTICAL = 1;
+    private static final int DIAGONAL = 2;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
-        int[][] a = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+        int n = Integer.parseInt(br.readLine().trim());
+        int[][] map = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) a[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= n; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
-        
-        long[][][] dp = new long[N + 1][N + 1][3];
-        if (a[1][1] == 0 && a[1][2] == 0) dp[1][2][0] = 1;
-        
-        for (int i = 1; i <= N; i++) {
-            for (int j = 3; j <= N; j++) {
-                if (a[i][j] == 1) continue;
-                dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
-                if (i > 1) dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
-                if (i > 1 && a[i - 1][j] == 0 && a[i][j - 1] == 0)
-                    dp[i][j][2] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+
+        // 이동했을 때 파이프가 특정 방향이 되는 경우의 수
+        // 가로/세로/대각선 방향의 파이프가 각각 이동할 수 있는 경로가 다르기에 구분해야 함
+        long[][][] dp = new long[3][n + 1][n + 1];
+        dp[HORIZONTAL][1][2] = 1;
+
+        for (int r = 1; r <= n; r++) {
+            for (int c = 1; c <= n; c++) {
+                if (map[r][c] == 1) {
+                    continue;
+                }
+
+                // 가로
+                dp[HORIZONTAL][r][c] += dp[HORIZONTAL][r][c - 1] + dp[DIAGONAL][r][c - 1];
+                // 세로
+                dp[VERTICAL][r][c] += dp[VERTICAL][r - 1][c] + dp[DIAGONAL][r - 1][c];
+                // 대각선
+                if (map[r][c - 1] != 1 && map[r - 1][c] != 1) {
+                    // 벽 조건을 만족하면,
+                    dp[DIAGONAL][r][c] += dp[HORIZONTAL][r - 1][c - 1] + dp[VERTICAL][r - 1][c - 1] + dp[DIAGONAL][r - 1][c - 1];
+                }
             }
         }
-        
-        long ans = dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
-        System.out.println(ans);
+
+        System.out.println(dp[HORIZONTAL][n][n] + dp[VERTICAL][n][n] + dp[DIAGONAL][n][n]);
     }
 }
