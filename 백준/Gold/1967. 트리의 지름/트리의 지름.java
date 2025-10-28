fe96e429b27fef0b1be604a32788fd9538eb64d0
이찬미 <anytime0224@gmail.com>
2025-10-28 15:18:28 +0900

    Update 트리의 지름.java

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -3,8 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+/**
+ * < 문제 분석 >
+ *     - 트리에서 각 노드로 연결되는 경로는 단 하나다.
+ *     - 트리의 모든 노드는 이어져 있다.
+ *     - 트리는 사이클이 없다.
+ *     - 따라서 임의의 점에서 가장 먼 점은 지름의 양 끝점 중 하나다. (U - A - B - C - D - V)
+ *     - 트리가 아닌 그래프에서는 각 노드로 연결되는 경로가 여러 개이므로 (사이클 등) 위 가정이 성립하지 않는다.
+ * < 구현 방법 >
+ *     - 임의의 점에서 가장 먼 노드를 먼저 찾는다. (지름의 양 끝점 중 하나)
+ *     - 해당 점에서 가장 먼 노드와의 거리를 구한다. (= 트리의 지름)
+ */
+
+class Path {
+    int node;
+    int distance;
+
+    public Path(int node, int distance) {
+        this.node = node;
+        this.distance = distance;
+    }
+}
 
 class Edge {
     int to;
@@ -18,7 +42,7 @@ class Edge {
 
 public class Main {
 
-    static int endpoint;
+    static int endpoint = -1;
     static int max = Integer.MIN_VALUE;
 
     static int n;
@@ -47,14 +71,23 @@ public class Main {
             tree[n2].add(new Edge(n1, weight));
         }
 
-        // endpoint 찾기
+        // dfs O(n - 1)
+        // 지름의 endpoint 찾기
         Arrays.fill(isVisited, false);
         isVisited[1] = true;
         dfs(1, 0);
 
+        // 지름의 길이 구하기
         Arrays.fill(isVisited, false);
         isVisited[endpoint] = true;
         dfs(endpoint, 0);
+
+        // dijkstra O(e * logv)
+        // 지름의 endpoint 찾기
+        dijkstra(1);
+
+        // 지름의 길이 구하기
+        dijkstra(endpoint);
 
         System.out.println(max);
     }
@@ -74,6 +107,30 @@ public class Main {
             if (total > max) {
                 endpoint = start;
                 max = total;
+            }
+        }
+    }
+
+    private static void dijkstra(int start) {
+        int[] dist = new int[n + 1];
+        PriorityQueue<Path> pq = new PriorityQueue<>(Comparator.<Path>comparingInt(p -> p.distance).reversed());
+        pq.add(new Path(start, 0));
+
+        while (!pq.isEmpty()) {
+            Path path = pq.poll();
+
+            for (Edge edge : tree[path.node]) {
+                if (dist[edge.to] >= path.distance + edge.weight) {
+                    dist[edge.to] = path.distance + edge.weight;
+                    pq.add(new Path(edge.to, dist[edge.to]));
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] > max) {
+                max = dist[i];
+                endpoint = i;
             }
         }
     }
