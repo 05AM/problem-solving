d00d4cad5d03e54b894f514004fa5f2fcd83628d
이찬미 <anytime0224@gmail.com>
2025-11-14 11:59:49 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
@@ -1,70 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Main {
-
-    private static int n, m;
-    private static int[] sequence1, sequence2;
-    private static StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(in.readLine());
-        sequence1 = Arrays.stream(in.readLine().split(" "))
-            .mapToInt(Integer::parseInt)
-            .toArray();
+        int maxValue = -1;
 
-        m = Integer.parseInt(in.readLine());
-        sequence2 = Arrays.stream(in.readLine().split(" "))
-            .mapToInt(Integer::parseInt)
-            .toArray();
+        int n = Integer.parseInt(in.readLine());
+        StringTokenizer st = new StringTokenizer(in.readLine());
 
-        solution();
-    }
-
-    private static void solution() {
-        // 주어진 수열에서 가장 큰 공통 원소를 찾는 것을 반복
-        // 한쪽이 남은 개수가 0이 되면 끝
-        int cnt = 0;
-        int[] fromIdx = new int[] {0, 0};
-
-        while (fromIdx != null && (fromIdx[0] < n && fromIdx[1] < m)) {
-            fromIdx = findBigCommonElementIdxs(fromIdx[0], fromIdx[1]);
-            if (fromIdx != null) {
-                result.append(sequence1[fromIdx[0] - 1]).append(" ");
-                cnt++;
-            }
+        int[] sequence1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            sequence1[i] = Integer.parseInt(st.nextToken());
+            maxValue = Math.max(maxValue, sequence1[i]);
         }
 
-        System.out.println(cnt);
-        System.out.println(result);
-    }
+        int m = Integer.parseInt(in.readLine());
+        st = new StringTokenizer(in.readLine());
 
-    private static int[] findBigCommonElementIdxs(int idx1, int idx2) {
-        // idx1 ~ n / idx2 ~ m 안에서 가장 큰 공통 원소 찾기
-        int max = Integer.MIN_VALUE;
-        int newIdx1 = idx1;
-        int newIdx2 = idx2;
+        int[] sequence2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            sequence2[i] = Integer.parseInt(st.nextToken());
+            maxValue = Math.max(maxValue, sequence2[i]);
+        }
 
-        for (int i = idx1; i < n; i++) {
-            for (int j = idx2; j < m; j++) {
-                if (sequence1[i] == sequence2[j]) {
-                    if (sequence1[i] > max) {
-                        max = sequence1[i];
-                        newIdx1 = i;
-                        newIdx2 = j;
-                    }
+        List<Integer> result = new ArrayList<>();
+        int startIdx1 = 0;
+        int startIdx2 = 0;
+
+        while (maxValue > 0) {
+            boolean hasValue1 = false;
+            boolean hasValue2 = false;
+            int tmpIdx1 = 0;
+            int tmpIdx2 = 0;
+
+            for (int i = startIdx1; i < n; i++) {
+                if (sequence1[i] == maxValue) {
+                    hasValue1 = true;
+                    tmpIdx1 = i;
+                    break;
                 }
             }
+
+            for (int i = startIdx2; i < m; i++) {
+                if (sequence2[i] == maxValue) {
+                    hasValue2 = true;
+                    tmpIdx2 = i;
+                    break;
+                }
+            }
+
+            if (hasValue1 && hasValue2) {
+                result.add(maxValue);
+                startIdx1 = tmpIdx1 + 1;
+                startIdx2 = tmpIdx2 + 1;
+                
+                continue;
+            }
+            
+            maxValue--;
         }
 
-        if (max != Integer.MIN_VALUE) {
-            return new int[] {newIdx1 + 1, newIdx2 + 1};
-        }
-
-        return null;
+        System.out.println(result.size());
+        System.out.println(
+            result.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "))
+        );
     }
 }
