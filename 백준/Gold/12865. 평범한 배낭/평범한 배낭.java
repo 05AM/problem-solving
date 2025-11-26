cc7dfcf7a1055347ac50149fe4ac46de824350bd
이찬미 <anytime0224@gmail.com>
2025-11-26 14:25:29 +0900

    Update 평범한 배낭.java

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,3 +1,40 @@
+// 정석 2차원 배열
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] weights = new int[n + 1];
+        int[] values = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            weights[i] = in.nextInt();
+            values[i] = in.nextInt();
+        }
+
+        // n: 현재까지 고려한 물건 번호, k: 현재 사용할 수 있는 배낭의 무게
+        int[][] dp = new int[n + 1][k + 1];
+
+        // 물건은 한 번만 담을 수 있으므로 물건 번호가 바깥 루프
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= k; w++) {
+                if (w < weights[i]) {
+                    dp[i][w] = dp[i - 1][w];
+                } else {
+                    dp[i][w] = Math.max(dp[i - 1][w - weights[i]] + values[i], dp[i - 1][w]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][k]);
+    }
+}
+
+
+// 1차원 배열
 import java.util.Scanner;
 
 public class Main {
@@ -27,3 +64,4 @@ public class Main {
         System.out.println(dp[K]);
     }
 }
+
