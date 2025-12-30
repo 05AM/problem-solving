c0c512d4f456b04ad3ed170628a72ebe2247579c
이찬미 <anytime0224@gmail.com>
2025-12-30 14:17:33 +0900

    [Gold V] Title: 치킨 배달, Time: 168 ms, Memory: 15260 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,81 +1,94 @@
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] dists;
-    static int m;
-    static int chickenCnt, houseCnt;
+    static class Coord {
+        int row;
+        int col;
+
+        public Coord(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    static int n, m;
+    static int[][] map;
+    static Coord[] selected;
+    static List<Coord> restaurants = new ArrayList<>();
+    static List<Coord> houses = new ArrayList<>();
+
     static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = in.nextInt();
-        m = in.nextInt();
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[n + 1][n + 1];
+        selected = new Coord[m];
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            map[i] = Arrays.stream(in.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
 
-        List<int[]> chickens = new ArrayList<>();
-        List<int[]> houses = new ArrayList<>();
-
-        for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= n; col++) {
-                map[row][col] = in.nextInt();
-
-                if (map[row][col] == 1) {
-                    houses.add(new int[] {row, col});
-                } else if (map[row][col] == 2) {
-                    chickens.add(new int[] {row, col});
+        // 치킨집, 집의 위치 구하기
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (map[r][c] == 1) {
+                    houses.add(new Coord(r, c));
+                } else if (map[r][c] == 2) {
+                    restaurants.add(new Coord(r, c));
                 }
             }
         }
 
-        houseCnt = houses.size();
-        chickenCnt = chickens.size();
-
-        // 각 집에서 각 치킨집까지의 거리 구하기
-        dists = new int[houseCnt][chickenCnt];
-
-        for (int row = 0; row < houseCnt; row++) {
-            int[] house = houses.get(row);
-            for (int col = 0; col < chickenCnt; col++) {
-                int[] chicken = chickens.get(col);
-                dists[row][col] = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
-            }
-        }
-
-        // 치킨집 chickenCnt개 중에 m개를 골랐을 때 치킨거리 구하고 최솟값 갱신하기
-        // dfs로 m개 고르기
-        // 도중 min보다 커진다면 그만 두기
-        int[] selected = new int[m];
-        dfs(0, 0, selected);
+        findMinChickenDistance(0, 0);
 
         System.out.println(min);
     }
 
-    private static void dfs(int start, int depth, int[] selected) {
-        if (depth == m) {
-            // 반복문을 돌며 도시의 치킨 거리 구하기
-            int cityDist = 0;
-            for (int house = 0; house < houseCnt; house++) {
-                int best = Integer.MAX_VALUE;
-
-                for (int chicken : selected) {
-                    best = Math.min(best, dists[house][chicken]);
+    private static void findMinChickenDistance(int selectIdx, int startIdx) {
+        if (selectIdx == m) {
+            int distance = calculateChickenDist();
+            min = Math.min(min, distance);
+        } else {
+            for (int i = startIdx; i < restaurants.size(); i++) {
+                if (restaurants.size() - startIdx < m - selectIdx) {
+                    break;
                 }
-                cityDist += best;
+
+                selected[selectIdx] = restaurants.get(i);
+                findMinChickenDistance(selectIdx + 1, i + 1);
+            }
+        }
+    }
+
+    private static int calculateChickenDist() {
+        int chickenDist = 0;
+
+        for (Coord house : houses) {
+            int min = Integer.MAX_VALUE;
+
+            for (Coord restaurant : selected) {
+                min = Math.min(min, calculateDist(house, restaurant));
             }
 
-            min = Math.min(min, cityDist);
-            return;
+            chickenDist += min;
         }
 
-        for (int i = start; i < chickenCnt; i++) {
-            selected[depth] = i;
-            dfs(i + 1, depth + 1, selected);
-        }
+        return chickenDist;
+    }
+
+    private static int calculateDist(Coord coord1, Coord coord2) {
+        return Math.abs(coord1.row - coord2.row) + Math.abs(coord1.col - coord2.col);
     }
 }
