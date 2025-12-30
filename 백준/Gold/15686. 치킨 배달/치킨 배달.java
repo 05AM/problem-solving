5cb3c4f4d1028b1b4b040a35470a994aa55821d7
이찬미 <anytime0224@gmail.com>
2025-12-30 14:41:03 +0900

    [Gold V] Title: 치킨 배달, Time: 144 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -18,12 +17,12 @@ public class Main {
     }
 
     static int n, m;
-    static int[][] map;
-    static Coord[] selected;
+    static int[][] dist;
+    static boolean[] chosen; // 치킨집 선택 여부
     static List<Coord> restaurants = new ArrayList<>();
     static List<Coord> houses = new ArrayList<>();
 
-    static int min = Integer.MAX_VALUE;
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -32,63 +31,71 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        selected = new Coord[m];
-        map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            map[i] = Arrays.stream(in.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-        }
-
-        // 치킨집, 집의 위치 구하기
         for (int r = 0; r < n; r++) {
+            st = new StringTokenizer(in.readLine());
             for (int c = 0; c < n; c++) {
-                if (map[r][c] == 1) {
+                int value = Integer.parseInt(st.nextToken());
+                if (value == 1) {
                     houses.add(new Coord(r, c));
-                } else if (map[r][c] == 2) {
+                } else if (value == 2) {
                     restaurants.add(new Coord(r, c));
                 }
             }
         }
 
-        findMinChickenDistance(0, 0);
+        chosen = new boolean[restaurants.size()];
 
-        System.out.println(min);
+        // 각 치킨집과 집 간의 거리 미리 구하기
+        dist = new int[houses.size()][restaurants.size()];
+        for (int i = 0; i < houses.size(); i++) {
+            Coord house = houses.get(i);
+            for (int j = 0; j < restaurants.size(); j++) {
+                Coord restaurant = restaurants.get(j);
+                dist[i][j] = Math.abs(house.row - restaurant.row)
+                        + Math.abs(house.col - restaurant.col);
+            }
+        }
+
+        findMinChickenDistance(0, 0);
+        System.out.println(answer);
     }
 
     private static void findMinChickenDistance(int selectIdx, int startIdx) {
         if (selectIdx == m) {
-            int distance = calculateChickenDist();
-            min = Math.min(min, distance);
-        } else {
-            for (int i = startIdx; i < restaurants.size(); i++) {
-                if (restaurants.size() - startIdx < m - selectIdx) {
-                    break;
-                }
+            answer = Math.min(answer, calculateChickenDist());
+            return;
+        }
 
-                selected[selectIdx] = restaurants.get(i);
-                findMinChickenDistance(selectIdx + 1, i + 1);
+        for (int i = startIdx; i < restaurants.size(); i++) {
+            // 남은 개수보다 선택해야 할 개수가 더 많으면,
+            if (restaurants.size() - i < m - selectIdx) {
+                break;
             }
+
+            chosen[i] = true;
+            findMinChickenDistance(selectIdx + 1, i + 1);
+            chosen[i] = false;
         }
     }
 
     private static int calculateChickenDist() {
         int chickenDist = 0;
 
-        for (Coord house : houses) {
-            int min = Integer.MAX_VALUE;
+        for (int houseIdx = 0; houseIdx < houses.size(); houseIdx++) {
+            int bestForHouse = Integer.MAX_VALUE;
 
-            for (Coord restaurant : selected) {
-                min = Math.min(min, calculateDist(house, restaurant));
+            for (int chickenIdx = 0; chickenIdx < restaurants.size(); chickenIdx++) {
+                if (!chosen[chickenIdx]) continue;
+                bestForHouse = Math.min(bestForHouse, dist[houseIdx][chickenIdx]);
             }
 
-            chickenDist += min;
+            chickenDist += bestForHouse;
+
+            if (chickenDist >= answer) {
+                return chickenDist;
+            }
         }
 
         return chickenDist;
-    }
-
-    private static int calculateDist(Coord coord1, Coord coord2) {
-        return Math.abs(coord1.row - coord2.row) + Math.abs(coord1.col - coord2.col);
     }
 }
