ede57adbde8b60da1d9386fd643db8038f4e46fa
이찬미 <anytime0224@gmail.com>
2026-01-27 23:23:34 +0900

    Refactor input handling and improve variable naming
    Refactored input handling to use BufferedReader and StringTokenizer for better performance. Updated variable names for clarity.

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,35 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt();
-        in.nextLine();
-        long[] distances = new long[n - 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+        long[] dists = new long[n - 1];
         long[] costs = new long[n];
 
+        StringTokenizer input = new StringTokenizer(in.readLine());
         for (int i = 0; i < n - 1; i++) {
-            distances[i] = in.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            costs[i] = in.nextInt();
+            dists[i] = Integer.parseInt(input.nextToken());
         }
 
-        System.out.println(solution(n, distances, costs));
+        input = new StringTokenizer(in.readLine());
+        for (int i = 0; i < n; i++) {
+            costs[i] = Integer.parseInt(input.nextToken());
+        }
+
+        long minTotal = solution(n, dists, costs);
+        System.out.println(minTotal);
     }
 
-    private static long solution(int n, long[] distances, long[] costs) {
+    private static long solution(int n, long[] dists, long[] costs) {
         long sum = 0;
         long currentMinCost = Integer.MAX_VALUE;
 
         for (int i = 0; i < n - 1; i++) {
-            if (costs[i] >= currentMinCost) {
-                sum += currentMinCost * distances[i];
-            } else {
+            if (costs[i] < currentMinCost) {
                 currentMinCost = costs[i];
-                sum += currentMinCost * distances[i];
             }
+            sum += currentMinCost * dists[i];
         }
 
         return sum;
