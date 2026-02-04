8aa1ea9c7c22ab85cb9a3aebd8dcd2a462c414c2
이찬미 <anytime0224@gmail.com>
2026-02-04 23:46:58 +0900

    [Gold III] Title: 웜홀, Time: 348 ms, Memory: 29408 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -6,79 +6,80 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+class Edge {
+    int from;
+    int to;
+    int cost;
 
-    static class Edge {
-        int u;
-        int v;
-        int weight;
-
-        public Edge(int u, int v, int weight) {
-            this.u = u;
-            this.v = v;
-            this.weight = weight;
-        }
+    public Edge(int from, int to, int cost) {
+        this.from = from;
+        this.to = to;
+        this.cost = cost;
     }
+}
+
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
 
-        int tc = Integer.parseInt(in.readLine());
-        while (tc-- != 0) {
+        while (t-- > 0) {
             StringTokenizer input = new StringTokenizer(in.readLine());
+
             int n = Integer.parseInt(input.nextToken());
             int m = Integer.parseInt(input.nextToken());
             int w = Integer.parseInt(input.nextToken());
 
             List<Edge> edges = new ArrayList<>();
-            // 도로 간선 초기화
             for (int i = 0; i < m; i++) {
                 input = new StringTokenizer(in.readLine());
+                int v1 = Integer.parseInt(input.nextToken());
+                int v2 = Integer.parseInt(input.nextToken());
+                int cost = Integer.parseInt(input.nextToken());
 
-                int u = Integer.parseInt(input.nextToken());
-                int v = Integer.parseInt(input.nextToken());
-                int weight = Integer.parseInt(input.nextToken());
-
-                edges.add(new Edge(u, v, weight));
-                edges.add(new Edge(v, u, weight));
+                edges.add(new Edge(v1, v2, cost));
+                edges.add(new Edge(v2, v1, cost));
             }
 
-            // 웜홀 간선 초기화
             for (int i = 0; i < w; i++) {
                 input = new StringTokenizer(in.readLine());
+                int v1 = Integer.parseInt(input.nextToken());
+                int v2 = Integer.parseInt(input.nextToken());
+                int cost = Integer.parseInt(input.nextToken());
 
-                int u = Integer.parseInt(input.nextToken());
-                int v = Integer.parseInt(input.nextToken());
-                int weight = Integer.parseInt(input.nextToken());
-
-                edges.add(new Edge(u, v, -weight));
+                edges.add(new Edge(v1, v2, -cost));
             }
 
-            String answer = hasNegativeCycle(n, edges) ? "YES" : "NO";
-            System.out.println(answer);
-        }
-    }
+            int[] dists = new int[n + 1];
+            Arrays.fill(dists, 0);
 
-    private static boolean hasNegativeCycle(int n, List<Edge> edges) {
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, 0);
+            boolean hasNegativeCycle = false;
+            for (int i = 1; i <= n; i++) {
+                boolean updated = false;
+                for (Edge edge : edges) {
+                    int dist = dists[edge.from] + edge.cost;
 
-        for (int i = 1; i <= n - 1; i++) {
-            for (Edge edge : edges) {
-                if (dist[edge.v] > dist[edge.u] + edge.weight) {
-                    dist[edge.v] = dist[edge.u] + edge.weight;
+                    if (dists[edge.to] > dist) {
+                        dists[edge.to] = dist;
+                        updated = true;
+
+                        if (i == n) {
+                            hasNegativeCycle = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!updated) {
+                    break;
+                }
+                if (hasNegativeCycle) {
+                    break;
                 }
             }
-        }
 
-        boolean hasNegativeCycle = false;
-        for (Edge edge : edges) {
-            if (dist[edge.v] > dist[edge.u] + edge.weight) {
-                hasNegativeCycle = true;
-                break;
-            }
+            System.out.println(hasNegativeCycle ? "YES" : "NO");
         }
-
-        return hasNegativeCycle;
     }
 }
